00be16beee1933369c0cb613d886b5e07990b60f
Praveen Raju(PRAJU2) <55208489+praju2@users.noreply.github.com>
2021-02-26 16:56:14 -0500

    Initial Setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DjiaPredictComponent } from './djia-predict/djia-predict.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [ { path: '', redirectTo: '/home', pathMatch: 'full' },
+{ path: 'home', component: HomeComponent },
+{ path: 'djia', component: DjiaPredictComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DjiaPredictComponent } from './djia-predict/djia-predict.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,   
+    DjiaPredictComponent
   ],
   imports: [
     BrowserModule,
